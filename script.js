f5ec5565e27bdc4da68491b3d980345ec4774ebf
ariabrauchli <42819915+ariabrauchli@users.noreply.github.com>
2025-09-04 19:32:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Change DOM property (text content) on button click
+const button = document.getElementById('changeTextBtn');
+const paragraph = document.getElementById('text');
+
+button.addEventListener('click', () => {
+    paragraph.textContent = "The text has been changed!";
+});
+
+// Change CSS property (background color) on mouseover
+const box = document.getElementById('colorBox');
+
+box.addEventListener('mouseover', () => {
+    box.style.backgroundColor = 'lightgreen';
+});
+
+box.addEventListener('mouseout', () => {
+    box.style.backgroundColor = 'lightblue';
+});
 
